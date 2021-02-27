a41d0fe3c714787b445738928f2460500f16ed1a
Patrick Jattke <pjattke@ethz.ch>
2021-02-27 08:49:00 +0100

    Removed ignored files

--- SEAL-master/CMakeFiles/seal.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libseal-3.5.a"
-)
--- SEAL-master/thirdparty/googletest/build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wonderwoman/desktop/SEAL_N/SEAL-master/thirdparty/googletest/src/googletest
-# Build directory: /home/wonderwoman/desktop/SEAL_N/SEAL-master/thirdparty/googletest/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
