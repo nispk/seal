b852e7e1f76ed0e5debb2fc3bbbc10f99d36024a
Patrick Jattke <pjattke@ethz.ch>
2021-02-27 08:41:21 +0100

    Replaces IntegerEncoder by BatchEncoder

--- SEAL-master/native/HE_functions/rotate.cpp
+++ SEAL-master/native/HE_functions/rotate.cpp
@@ -17,7 +17,7 @@ std::unique_ptr<seal::RelinKeys> rk;
 vector<Ciphertext> rotate_right(shared_ptr<SEALContext> context, vector<Ciphertext> x, int64_t s)
 {
     Decryptor decryptor(context, *sk);
-    IntegerEncoder intencoder(context);
+    BatchEncoder batchEncoder(context);
 
     for (int64_t i = 0; i < s; i++)
     {
@@ -29,7 +29,9 @@ vector<Ciphertext> rotate_right(shared_ptr<SEALContext> context, vector<Cipherte
     {
         Plaintext plain1;
         decryptor.decrypt(x[q], plain1);
-        cout << intencoder.decode_int64(plain1);
+        std::vector<int64_t> vec;
+        batchEncoder.decode(plain1, vec);
+        cout << vec.at(0);
         cout << endl;
         plain1.release();
     }
@@ -42,7 +44,7 @@ void rotate_mult(shared_ptr<SEALContext> context, Ciphertext input)
     Encryptor encryptor(context, *pk);
     Evaluator evaluator(context);
     Decryptor decryptor(context, *sk);
-    IntegerEncoder intencoder(context);
+    BatchEncoder batchEncoder(context);
     vector<Plaintext> x_plain;
     vector<Ciphertext> x_enc;
     vector<Plaintext> func_indices;
@@ -57,24 +59,33 @@ void rotate_mult(shared_ptr<SEALContext> context, Ciphertext input)
    
     Plaintext decrp;
     decryptor.decrypt(input, decrp);
-    cout << " The input to this function is " << intencoder.decode_int64(decrp) << endl;
+    std::vector<int64_t> vec;
+    batchEncoder.decode(decrp, vec);
+    cout << " The input to this function is " << vec.at(0) << endl;
     cout << endl;
 
     Plaintext max_input_plain;
-    intencoder.encode(max_input_range, max_input_plain);
+    std::vector<int64_t> mir = {max_input_range};
+    batchEncoder.encode(mir, max_input_plain);
     evaluator.add_plain(input, max_input_plain, temp);
     max_input_plain.release();
 
     Plaintext dec;
     decryptor.decrypt(temp, dec);
-    cout << " temp value after adding max_input_plain " << intencoder.decode_int64(dec) << endl;
+    vec.clear();
+    batchEncoder.decode(dec, vec);
+    cout << " temp value after adding max_input_plain " << vec.at(0) << endl;
     
-
     cout << " Compute indices vector,test input vector and the encrypted x vector" << endl;
     for (int j = 0; j <= N; j++)
     {
-        x_enc.push_back(temp);  // = 2 
-        func_indices.push_back(intencoder.encode(j)); // = 0, 1, ..., 16
+        x_enc.push_back(temp);  // = 2
+        vec.clear();
+        vec.push_back(j);
+        Plaintext ptxt;
+        batchEncoder.encode(vec, ptxt);
+        // func_indices.push_back(batchEncoder.encode(j)); // = 0, 1, ..., 16
+        func_indices.push_back(ptxt); // = 0, 1, ..., 16
         evaluator.sub_plain_inplace(x_enc[j], func_indices[j]); // 2-0, 2-1, 2-2, ... , 2-16
     }
 
@@ -93,22 +104,29 @@ void rotate_mult(shared_ptr<SEALContext> context, Ciphertext input)
             evaluator.multiply_inplace(x_enc[i], rot_x[i]);
             evaluator.relinearize_inplace(x_enc[i], *rk);
             decryptor.decrypt(x_enc[i], a3);
-            cout << intencoder.decode_int64(a1) << " * " << intencoder.decode_int64(a2) << "  =  " << intencoder.decode_int64(a3) << endl;
+
+            std::vector<int64_t> a1_val;
+            batchEncoder.decode(a1, a1_val);
+            std::vector<int64_t> a2_val;
+            batchEncoder.decode(a2, a2_val);
+            std::vector<int64_t> a3_val;
+            batchEncoder.decode(a3, a3_val);
+            cout << a1_val.at(0) << " * " << a2_val.at(0) << "  =  " << a3_val.at(0) << endl;
         }
+    
 
-
-        // Plaintext decd;
-        // cout << endl;
-        // cout << " rotation input is " << endl;
-        // for (int i = 0; i <= N; i++)
-        // {
-        //     decryptor.decrypt(x_enc[i], decd);
-        //     cout << intencoder.decode_int64(decd) << endl;
-        // }
-        // cout << endl;
+        Plaintext decd;
+        cout << endl;
+        cout << " rotation input is " << endl;
+        for (int i = 0; i <= N; i++)
+        {
+            decryptor.decrypt(x_enc[i], decd);
+            vec.clear();
+            batchEncoder.decode(decd, vec);
+            cout << vec.at(0) << endl;
+        }
+        cout << endl;
     }
-
-    exit(0);
 
     cout << " Noise in product x_enc vector is " << endl;
     for (int i = 0; i <= N; i++)
@@ -132,7 +150,9 @@ int main()
     size_t poly_modulus_degree = 32768;
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-    parms.set_plain_modulus(12289);
+    // parms.set_plain_modulus(12289);
+    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
+    // parms.set_plain_modulus(8192);
     auto context = SEALContext::Create(parms);
     cout << "Set encryption parameters and print" << endl;
     print_parameters(context);
@@ -144,14 +164,16 @@ int main()
     Encryptor encryptor(context, *pk);
     Evaluator evaluator(context);
     Decryptor decryptor(context, *sk);
-    IntegerEncoder encoder(context);
+    BatchEncoder encoder(context);
     
     int64_t v1 = -2;
     int64_t v2 = 3;
     Plaintext v_plain1, v_plain2;
     Ciphertext v_enc1, v_enc2;
-    encoder.encode(v1, v_plain1);
-    encoder.encode(v2, v_plain2);
+    std::vector<int64_t> val1 = {v1};
+    std::vector<int64_t> val2 = {v2};
+    encoder.encode(val1, v_plain1);
+    encoder.encode(val2, v_plain2);
     encryptor.encrypt(v_plain2, v_enc2);
     encryptor.encrypt(v_plain1, v_enc1);
     evaluator.multiply_inplace(v_enc1, v_enc2);
